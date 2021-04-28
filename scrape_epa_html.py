38ae34c5bd6bcf8fefd60b82f883b0fa3287657f
Avery Richards <43361193+Averysaurus@users.noreply.github.com>
2021-04-28 11:02:50 -0700

    Update scrape_epa_html.py

--- scrape_epa_html.py
+++ scrape_epa_html.py
@@ -6,43 +6,41 @@ Created on Sat Jan 16 01:09:57 2021
 @author: avery
 """
 
-
 # import required modules
 import requests
 from bs4 import BeautifulSoup
-
 import pandas as pd
 import numpy as np
 
-
-
-
 # make a GET request
 req = requests.get('https://www.epa.gov/navajo-nation-uranium-cleanup/abandoned-mines-cleanup-site-screen-reports')
-# read the content of the server’s response
+
+# read content of the server’s response
 src = req.text
 
-# parse the response into an HTML tree
+# parse response into an HTML tree
 soup = BeautifulSoup(src, 'lxml')
-# take a look
-#print(soup.prettify()[:3000])
 
+# take a look
+print(soup.prettify()[:3000])
+
+# extract rows
 rows = soup.find_all("tbody")
 print(rows[0].prettify())
 
-
+# evaluate rows
 len(rows)
 rows
 
-
-
-# select only those 'td' tags.
+# select only  'td' tags.
 row = rows[0]
 detailCells = row.select('td')
 
+# evaluate 
 len(detailCells)
 detailCells[0]
 
+# extract data
 rowData = [cell.text for cell in detailCells]
 
 # check em out
@@ -51,17 +49,17 @@ print(rowData[1]) # Description
 print(rowData[2]) # Region
 print(rowData[3]) # Chapter
 
+# create empty object
 mines = []
 
 rows = soup.select('td')
 len(rows)
-
 rowData = [cell.text for cell in rows]
 
+# for loop to iterate over objects
 for row in rows:
     
     #detailCells = row.select('td')
-       
     rowData = [cell.text for cell in rows]
     
     # Collect information
@@ -75,32 +73,21 @@ for row in rows:
     
     # Append to list
     mines.append(tup)
- 
+
+# evaluate
 len(mines)  
-  
+
+# structure into DataFrame
 df_one = pd.DataFrame(np.array(rowData).reshape(476,4))
-    
 df_test = pd.DataFrame(mines,columns=('alt_name', 
                                       'description',
                                       'region', 
                                       'chapter'))
 
   
-
+# output to .csv
 df_one.to_csv("mine_output_test.csv") 
     
     
-    # this is a list
-soup.select("a.sidemenu")
-
-# we first want to get an individual tag object
-first_link = soup.select("a.sidemenu")[0]
-
-# check out its class
-type(first_link)
-
-print(first_link.text)
-
-# SOLUTION
-[link['href'] for link in soup.select("a.mainmenu")]
+#~fin
 
